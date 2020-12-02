ed72ef5e6296d1a5b06d09fdf6396fadd1de4b20
Clodagh Walsh <42538837+clodaghwalsh17@users.noreply.github.com>
2020-12-02 22:46:24 +0000

    Add files via upload

--- CSV.java
+++ CSV.java
@@ -1,50 +1,41 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
+import com.opencsv.*;
 
 public class CSV {
-	/** Utility class for reading and writing to CSV files*/
-	
-	public static void writeToFile(String file,String[] content) {
-		try{
-            FileWriter writer = new FileWriter(file,true);
-            BufferedWriter buffer = new BufferedWriter(writer);
-            PrintWriter print = new PrintWriter(buffer);
-            String out = Arrays.toString(content).replaceAll("[\\[\\]]", "");
-            print.println(out);
-            print.close();
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
-	}
-	
 	public static ArrayList<String[]> readFromFile(String file) {
 		ArrayList<String[]> data = new ArrayList<String[]>();
-		String dataLine;
-        try{
-            BufferedReader buffer = new BufferedReader(new FileReader(file));
-            
-            while((dataLine = buffer.readLine()) != null)
-            {
-                data.add(dataLine.split(","));
-            }
-            buffer.close();
-        }
-        catch(FileNotFoundException e){
-            System.out.println("Couldn't find file");
-            e.printStackTrace();
-        }
-        catch(IOException e){
-            System.out.println("Couldn't read file");
-            e.printStackTrace();
-        }
-        return data;
+		String[] dataLine;
+		try {
+			
+			CSVReaderBuilder customReader = new CSVReaderBuilder(new FileReader(file));
+			CSVParserBuilder customParser = new CSVParserBuilder();
+			customReader.withSkipLines(1);
+			customParser.withIgnoreQuotations(false);
+			customReader.withCSVParser(customParser.build());
+			
+			CSVReader csv = customReader.build();
+
+			while ((dataLine = csv.readNext()) != null) {
+				data.add(dataLine);
+			}
+			csv.close();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return data;
+	}
+	
+	public static void writeToFile(String file,String[] content) {
+		try {
+			CSVWriter writer = new CSVWriter(new FileWriter(file,true));
+			writer.writeNext(content,false);
+			writer.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
