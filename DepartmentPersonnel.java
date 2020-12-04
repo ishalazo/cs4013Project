8d366eaea4486e6dc6bd2abe99e1fa7aa4864a75
TitoEt <72010582+TitoEt@users.noreply.github.com>
2020-12-04 00:14:13 +0000

    Create DepartmentPersonnel.java

---
+++ DepartmentPersonnel.java
@@ -0,0 +1,49 @@
+package cs4013Project;
+import java.util.ArrayList;
+
+
+//Do statistics in here
+//Statistics = total houses that paid in a particular area and the total tax located in a certain area
+//Maybe list all registered in area and find a % of those paid
+//Eircode routing thing 
+//Eircode is 7 characters long
+//First 3 characters are the routing key to identify the area
+
+public class DepartmentPersonnel {
+	private ArrayList <Payment> unpaid;
+	private String id;
+	private String ownerpayments;
+	private String propertypayments;
+	
+	/** DepartmentPersonnel
+	 * @author Tito Etimiri 19248547
+	 */
+	/**
+	 * @param id
+	 * @param owner
+	 * 
+	 */
+	
+	public DepartmentPersonnel() {
+		
+	}
+	
+	public void setOwnerPayments(String ownerpayments) {
+		
+	}
+	
+	public void setPropertyPayments(String propertypayments) {
+		
+	}
+	
+	public String getOwnerPayments() {
+		return ownerpayments;
+	}
+	
+	public String getPropertyPayments() {
+		return propertypayments;
+	}
+	
+//Get the property statistics for any area (eircode)
+	
+}
