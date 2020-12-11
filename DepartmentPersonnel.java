a08e6317f31e3fbc95540fd9a82f1717990ed4b1
TitoEt <72010582+TitoEt@users.noreply.github.com>
2020-12-11 23:43:41 +0000

    Update DepartmentPersonnel.java

--- DepartmentPersonnel.java
+++ DepartmentPersonnel.java
@@ -23,6 +23,8 @@ public class DepartmentPersonnel {
 	/*return payments made by a particular owner*/
 	public String getOwnerPayments(String ownerid) {
 		property = new ArrayList<Property>();
+		Utilities.filter(properties, "Ownerid", ownerid);
+		 Utilities.readFromFile("taxPayments.csv");
 		
 		for(int i = 0; i < property.size(); i++) {
 			if(ownerid.equals((property.get(i).getownerID()))){
@@ -35,6 +37,8 @@ public class DepartmentPersonnel {
 	/*return payments mad on a particular property*/
 	public String getPropertyPayments(String eircode) {
 		property = new ArrayList<Property>();
+		Utilities.filter(properties, "Eircode", eircode);
+		 Utilities.readFromFile("taxPayments.csv");
 		
 		for(int i = 0; i < property.size(); i++) {
 			if(eircode.equals((property.get(i).getEircode()))) {
@@ -44,25 +48,21 @@ public class DepartmentPersonnel {
 		return "Incorrect eircode";
 	}
 	
+	/*return all unpaid taxpayments for a particular year*/
+	public String getYearOverdues(int year) {
+		Utilities.filter(properties, "Year", year);
+		Utilities.readFromColumn("taxPayments.csv", 4)
+
+		 return "Overdue payments for the year: ";
+	}
 	
-	public ArrayList<TaxRecord> getYearOverdues(int year) {
-		ArrayList<TaxRecord> overdue = new ArrayList<>();
-		
-		for(int i = 0; i < property.size(); i++) {
-			if(property.get(i).isPaid == false){
-				return overdue;
-				}
-				
-			}
-		return overdue;
-		}
-		
 	//just return all the tax details for an area depending on the imputed routing key
 	public String getEircodeStats(String eircode) {
 		System.out.println("Enter the routing key");
-        
-		return "";
+		Utilities.filter(properties, "Eircode", eircode);
+		 Utilities.readFromFile("taxPayments.csv");
+		//total payments/amount of properties
+		return "\nAverage for area: ";
 	}
 	
    
-}
