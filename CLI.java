9274e25d8ba54a4265ee63365f89b48b8cf88750
ishalazo <58404081+ishalazo@users.noreply.github.com>
2020-12-12 18:26:24 +0000

    Add files via upload

--- CLI.java
+++ CLI.java
@@ -2,83 +2,96 @@ import java.util.*;
 
 public class CLI {
 	private Scanner scan;
-	
+	boolean more;
 	/**
 	 * Constructs the Command Line Interface
 	 */
 	public CLI() {
+		more = true;
 		scan = new Scanner(System.in);
 		TaxCalculator.setCalculator();
-//		ArrayList<Object> properties = fileToArrayList(Utilities.readFromFile("properties.csv"));
-//		ArrayList<Object> users = fileToArrayList(Utilities.readFromFile("systemLogins.csv"));
+		//		ArrayList<Object> properties = fileToArrayList(Utilities.readFromFile("properties.csv"));
+		//		ArrayList<Object> users = fileToArrayList(Utilities.readFromFile("systemLogins.csv"));
 	}
-	
+
 	//idk if i even need this 
-	private ArrayList<Object> fileToArrayList(ArrayList<String[]> file) {
-		String lastHead = file.get(0)[file.get(0).length-1];
+	public ArrayList<Object> fileToArrayList(String fileName) {
+		ArrayList<String[]> file = Utilities.readFromFile(fileName);
 		ArrayList<Object> output = new ArrayList<Object>();		
-		if(lastHead.equalsIgnoreCase("principal residence")) {
-			for(String[] row : file) {
+		if(fileName.equals("properties.csv")) {
+			for(int i = 1; i < file.size(); i++) {
+				String[] row = file.get(i);
 				Property p = new Property(row[0], row[1], row[2], row[3], 
-						Double.parseDouble(row[4]), Boolean.parseBoolean(row[5]));
+						Double.parseDouble(row[4]), Boolean.parseBoolean(row[5]),false);
 				output.add(p);
 			}
-		} else if(lastHead.equalsIgnoreCase("full name")) {
-			for(String[] row : file) {
-				Owner o = new Owner(row[2], row[0], row[1]);
+		} else if(fileName.equals("systemLogins.csv")) {
+			for(int i = 1; i < file.size(); i++) {
+				String[] row = file.get(i);
+				Owner o = new Owner(row[0], row[1], row[2]);
 				output.add(o);
 			}
 		}
 		return output;
 	}
-	
+
 	public void run() {
-		boolean more = true;
 		
+
 		while(more) {
-			System.out.println("O)wner\tD)epartment Personnel\tQ)uit");
+			System.out.println("O)wner  D)epartment Personnel  Q)uit");
 			String command = scan.nextLine().toUpperCase();
-			
-			if(command.equals("O")) {
-				System.out.println("L)ogin\tR)egister\tQ)uit");
-				String in = scan.nextLine().toUpperCase();
-				if(in.equals("L")) { //login
-					boolean successfulLogin = login();
-					if(!successfulLogin) {
+
+			switch(command) {
+			case "O": 
+				System.out.println("L)ogin  R)egister  Q)uit");
+				String OwnerIn = scan.nextLine().toUpperCase();
+				switch(OwnerIn) { //login
+					case "L":
+						boolean successfulLogin = login();
+						if(!successfulLogin) {
+							more = false;
+						} else {
+							System.out.println("Successful login");
+							//owner screen
+						}
+						break;
+						
+					case "R":	//Register
+						System.out.println("Are you eligible to register as an owner? Y/N");
+						String in1 = scan.nextLine().toUpperCase();
+						if(in1.equals("N")) {
+							more = false;
+						} else {
+							registerOwner();
+						}
+						break;
+						
+					case "Q": //Quit
 						more = false;
-					} else {
-						System.out.println("Successful login");
-						//owner screen
-					}
+						break;
 				}
-				else if(in.equals("R")) { //register owner
-					System.out.println("Are you eligible to register as an owner? Y/N");
-					String in1 = scan.nextLine().toUpperCase();
-					if(in1.equals("N")) {
-						more = false;
-					} else {
-						registerOwner(); //!!!!!!! bro are we redirecting them or asking for a restart
-					}
-				} 
-				else if(in.equals("Q")) { //quit
-					more = false;
-				}
-			}
-			else if(command.equals("D")) { //dept personell
+				break;
+
+			case "D": //Dept. Personnel
 				boolean successfulLogin = login();
 				if(!successfulLogin) {
-					more = false;
+					System.out.println("Unsuccessful login. Please try again.");
 				} else {
 					System.out.println("Successful login");
-					//employee screen
+					deptMenu();
 				}
-			}
-			else if(command.equals("Q")) { //quit
+				break;
+
+			case "Q": //Quit
 				more = false;
+				break;
 			}
+
+
 		}
 	}
-	
+
 	private boolean login() {
 		boolean success = false;
 		System.out.println("ID: ");
@@ -91,16 +104,66 @@ public class CLI {
 		}
 		return success;
 	}
-	
+
 	private void registerOwner() {
 		ArrayList<String> currentOwners = Utilities.readFromColumn("systemLogins.csv", 0);
 		int ownerID = Integer.parseInt(currentOwners.get(currentOwners.size() -1)) + 1;
-		System.out.println("Provide your full name:");
+		System.out.println("Enter your full name:");
 		String name = scan.nextLine();
-		System.out.println("Provide your password:");
+		System.out.println("Enter your password:");
 		String pw = scan.nextLine();
 		Utilities.writeToFile("systemLogins.csv", new String[] {Integer.toString(ownerID), pw, name});
 		System.out.println("You have successfully registered.\nPlease note your Owner ID: "+ownerID
-				+"\nPlease login with your new account");
+				+"\nPlease login with your new account.");
+	}
+	
+	private void ownerMenu() {
+		boolean more = true;
+		while(more) {
+			System.out.println("R)V)iew properties");
+		}
+	}
+	
+	private void deptMenu() {
+		boolean more = true;
+		while(more) {
+			System.out.println("P)roperty tax payments    "
+					+ "Ow)ner tax payments    "
+					+ "Ov)erdue property taxes    "
+					+ "T)ax statistics    "
+					+ "S)et tax calculator    "
+					+ "Q)uit");
+			String in = scan.nextLine().toUpperCase();
+			
+			switch(in) {
+				case "P":
+					System.out.println("Enter the eircode of the property");
+					String eircode = scan.nextLine();
+					String[] pInfo = Utilities.filter(Utilities.readFromFile("properties.csv"), "Eircode", eircode).get(1);
+					Property p = new Property(pInfo[0],pInfo[1],pInfo[2],pInfo[3],Double.parseDouble(pInfo[4]),Boolean.parseBoolean(pInfo[5]), false);
+					System.out.println("Owner ID: " + p.getOwnerID());
+					System.out.println(p.propBalStatement());
+					break;
+					
+				case "OW":
+					System.out.println("Enter Owner ID");
+					break;
+					
+				case "OV":
+					break;
+					
+				case "T":
+					break;
+				
+				case "S":
+					break;
+					
+				case "Q":
+					this.more = false;
+					more = false;
+					break;
+			}
+		}
+		
 	}
 }
--- Property.java
+++ Property.java
@@ -8,7 +8,6 @@ public class Property {
 	private String address, eircode, location,ownerID;
 	private double marketValue;
 	private boolean principalResidence;
-	private ArrayList<TaxRecord> taxes;
 
 	/**Constructs a Property Object
 	 * @param address address of property
@@ -19,24 +18,50 @@ public class Property {
 	 * @param principalResidence true if the property is the principal private property of the ownerID
 	 */
 	public Property(String ownerID, String address, String eircode, String location, 
-			double marketValue, boolean principalResidence) {
+			double marketValue, boolean principalResidence, boolean writeToCSV) {
 		this.ownerID = ownerID;
 		this.address = address;
 		this.eircode = eircode;
 		this.location = location;
 		this.marketValue = marketValue;
 		this.principalResidence = principalResidence;
-		taxes = new ArrayList<TaxRecord>();
-		calculatePropertyTax(true);
-		String[] info = {ownerID, address, eircode, location, Double.toString(marketValue), Boolean.toString(principalResidence)};
-		Utilities.writeToFile("properties.csv", info);
+		
+		
+		if(writeToCSV) { 
+			String[] info = {ownerID, address, eircode, location, Double.toString(marketValue), Boolean.toString(principalResidence)};
+			Utilities.writeToFile("properties.csv", info);
+			
+			//generate the current years tax cause it's registered in 2020
+			String[] c = {
+				eircode, 
+				ownerID, 
+				Integer.toString(LocalDate.now().getYear()), 
+				Double.toString(TaxCalculator.calculateTax(this)), 
+				Boolean.toString(false)};
+			Utilities.writeToFile("taxPayments.csv", c);
+		}
 	}
+
+//	private boolean insideCSV(String[] info) {
+//		ArrayList<String[]> CSVprops = Utilities.readFromFile("properties.csv");
+//		boolean inside = false;
+//		for(int i = 1; i < CSVprops.size(); i++) {
+//			String[] row = CSVprops.get(i);
+//			if(ownerID.equals(row[0]) && address.equalsIgnoreCase(row[1]) && eircode.equalsIgnoreCase(row[2]) && location.equalsIgnoreCase(row[3]) 
+//					&& marketValue == Double.parseDouble(row[4]) && principalResidence == Boolean.parseBoolean(row[5])) {
+//				inside = true;
+//				break;
+//			}
+//		}
+//		return inside;
+//	}
+
 
 	/**
 	 * Gets the name of the ownerID.
 	 * @return Name of the ownerID.
 	 */
-	public String getownerID() {
+	public String getOwnerID() {
 		return ownerID;
 	}
 
@@ -79,19 +104,16 @@ public class Property {
 	public String getAddress() {
 		return address;
 	}
-	
-	public ArrayList<TaxRecord> getTaxes() {
-		return taxes;
-	}
 
-	public void calculatePropertyTax(boolean isPaid) {
+
+	public void calculateCurrentTax(boolean isPaid) {
 		ArrayList<String[]> payments = Utilities.filter(Utilities.readFromFile("taxPayments.csv"), "Eircode", eircode);
 //		the code below sucks cause it assumes that there would always be a payment for this property in the
 //		csv but that's not always the case, specially if we're calling this inside the constructor
 //		that's why i have this take in a boolean as an argument so we can set it to true at the construction
 //		boolean isPaid = Boolean.parseBoolean(payments.get(payments.size()-1)[Utilities.indexCol(payments.get(0),"Paid")]);
 		if(!isPaid) {
-			double prev = Double.parseDouble(payments.get(payments.size()-1)[Utilities.indexCol(payments.get(0),"Tax")]);
+			double prev = Double.parseDouble(payments.get(payments.size()-1)[3/*Utilities.indexCol(payments.get(0),"Tax")*/]);
 			String[] content = {
 					eircode, 
 					ownerID, 
@@ -109,9 +131,22 @@ public class Property {
 			Utilities.writeToFile("taxPayments.csv", c);
 		}
 	}
+	
+	public String propBalStatement() {
+		ArrayList<String[]> propPayments = Utilities.filter(Utilities.readFromFile("taxPayments.csv"), "Eircode", eircode);
+		String output = "";
+		for(int i = 1; i < propPayments.size(); i++) {
+			String[] p = propPayments.get(i);
+			output += p[2] + " Tax: €" + p[3]+ ((Boolean.parseBoolean(p[4]))? ", paid\n":", overdue\n");
+			if(Integer.parseInt(p[2]) == LocalDate.now().getYear() && !Boolean.parseBoolean(p[4])) {
+				output += ", overdue";
+			}
+		}
+		return output;
+	}
 
 	public String toString() {
-		return "ownerID:" + ownerID + "\nAddress:\n" + address + "\n" + eircode 
+		return "Owner ID:" + ownerID + "\nAddress:" + address + "\nEircode: " + eircode 
 				+ "\nLocation: " + location 
 				+ "\nEstimated Market Value: €" + String.format("%.2f", marketValue) 
 				+ "\nPrincipal Private Residence: " + principalResidence;
