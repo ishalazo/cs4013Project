a220a647bbb512b62fb8943b8f2cf86c94ed7e9d
TitoEt <72010582+TitoEt@users.noreply.github.com>
2020-12-13 02:26:09 +0000

    Update DepartmentPersonnel.java

--- DepartmentPersonnel.java
+++ DepartmentPersonnel.java
@@ -1,25 +1,38 @@
 package cs4013Project;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DepartmentPersonnel {
 	
 	private ArrayList<Property> property;
 	private String eircode;
 	private String ownerid;
+	private ArrayList<String[]> properties;
+	private int id;
 	
 	/** DepartmentPersonnel
 	 * @author Tito Etimiri 19248547
 	 */
 	/**
-	 * @param ownerdata
-	 * @param propertydata
+	 * @param eircode
+	 * @param ownerid
 	 * 
 	 */
 	
-	public DepartmentPersonnel() {
+	public DepartmentPersonnel(int id) {
+		//departmentPersonnel have the same four digit id
+		this.id = id;
 
 	}
 	
+	//some of the methods are a lil redundant now but i don't want to delete anything yet
+	
+	private ArrayList<String[]> getOwnerProperties(String ownerid) { 
+        return Utilities.filter(Utilities.readFromFile("properties.csv"), "Owner_id",ownerid);
+    }
+	
+	//seems? kinda? idk, not gonna delete ~for review ig~
 	/*return payments made by a particular owner*/
 	public String getOwnerPayments(String ownerid) {
 		property = new ArrayList<Property>();
@@ -27,14 +40,15 @@ public class DepartmentPersonnel {
 		 Utilities.readFromFile("taxPayments.csv");
 		
 		for(int i = 0; i < property.size(); i++) {
-			if(ownerid.equals((property.get(i).getownerID()))){
+			if(ownerid.equals((property.get(i).getOwnerID()))){
 				return(property.get(i).toString());
 			}
 		}
 		return "Incorrect id";
 	}
 	
-	/*return payments mad on a particular property*/
+	//seems? kinda? idk, not gonna delete ~for review ig~
+	/*return payments made on a particular property*/
 	public String getPropertyPayments(String eircode) {
 		property = new ArrayList<Property>();
 		Utilities.filter(properties, "Eircode", eircode);
@@ -48,21 +62,30 @@ public class DepartmentPersonnel {
 		return "Incorrect eircode";
 	}
 	
-	/*return all unpaid taxpayments for a particular year*/
-	public String getYearOverdues(int year) {
-		Utilities.filter(properties, "Year", year);
-		Utilities.readFromColumn("taxPayments.csv", 4)
-
-		 return "Overdue payments for the year: ";
-	}
+	/*return all unpaid tax payments for a particular owner, on a particular year*/
+	private ArrayList<String> getOverdues(String ownerid){
+        ArrayList<String[]> properties = Utilities.filter(Utilities.filter(Utilities.readFromFile("taxPayments.csv"),"Year",ownerid), "Eircode", eircode);
+        properties = Utilities.filter(properties, "Year", LocalDate.now().getYear());
+        ArrayList<String[]> unpaid = Utilities.filter(properties,"Paid",false);
+        return Utilities.readFromColumn(unpaid,0);
+    }
 	
-	//just return all the tax details for an area depending on the imputed routing key
-	public String getEircodeStats(String eircode) {
-		System.out.println("Enter the routing key");
-		Utilities.filter(properties, "Eircode", eircode);
-		 Utilities.readFromFile("taxPayments.csv");
-		//total payments/amount of properties
-		return "\nAverage for area: ";
-	}
+	public String getRoutingKey(String eircode){        
+        return eircode.substring(0, 2);
+    }
 	
-   
+	public String getPropertStats(String eircode) {
+		
+		//a lil error with filters here, but mist likely because i didnt do it correctly
+		double totalpaid = Utilities.filter(Utilities.readFromColumn("taxPayments.csv", 5), "Eircode",eircode);
+		
+		double allprop = Utilities.filter(Utilities.readFromColumn("properties.csv", 3), "Eircode",eircode);
+		
+		String RKey = getRoutingKey(eircode);
+		
+		return "Eircode: " + eircode + "\nTotal Tax Paid: " + String.format( "%.2f", totalpaid) + "\nAverage Tax Paid: " + String.format( "%.2f", (totalpaid / allprop));
+		   
+		  
+	
+	}
+}
